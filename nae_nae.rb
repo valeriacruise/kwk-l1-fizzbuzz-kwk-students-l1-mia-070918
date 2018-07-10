61acdbd92fd5508ceaec307847e1e6629a065284
valeriacruise <valeriacruise22@gmail.com>
2018-07-10 18:41:48 +0000

    automatically backed up by learn

--- nae_nae.rb
+++ nae_nae.rb
@@ -1,5 +1,7 @@
-loop do
-  puts "watch me whip"
-  puts "watch me nae nae"
-  break
+def hungry_lei
+  sour_belts= 0
+  while sour_belts< 30
+  puts "GIVE ME MORE SOUR BELTS"
+  sour_belts +=1
+  end
 end
